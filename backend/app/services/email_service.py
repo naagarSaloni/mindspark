bc0fac0a7739970fc80342f4ece51887db09d68d
Saloni <saloninaagar08@gmail.com>
2026-06-16 19:06:35 +0530

    add resend email otp

--- backend/app/services/email_service.py
+++ backend/app/services/email_service.py
@@ -1,47 +1,27 @@
-import os
-import smtplib
-from email.mime.text import MIMEText
-from dotenv import load_dotenv
+import requests
 
-# Load environment variables
-load_dotenv()
-
-EMAIL = os.getenv("EMAIL")
-APP_PASSWORD = os.getenv("APP_PASSWORD")
+RESEND_API_KEY = "re_PHNuLRKX_Jd26oi6mBxKb2hyz7Biitwo6"
 
 
 def send_otp_email(receiver_email: str, otp: str):
     try:
-        subject = "MindSpark Password Reset OTP"
+        response = requests.post(
+            "https://api.resend.com/emails",
+            headers={
+                "Authorization": f"Bearer {RESEND_API_KEY}",
+                "Content-Type": "application/json"
+            },
+            json={
+                "from": "MindSpark <onboarding@resend.dev>",
+                "to": receiver_email,
+                "subject": "MindSpark OTP",
+                "html": f"<h2>Your OTP is: {otp}</h2>"
+            }
+        )
 
-        body = f"""
-Hello,
-
-Your MindSpark OTP is:
-
-{otp}
-
-This OTP is valid for 5 minutes.
-
-If you did not request this password reset, ignore this email.
-
-MindSpark Team
-"""
-
-        # Create email message
-        msg = MIMEText(body)
-        msg["Subject"] = subject
-        msg["From"] = EMAIL
-        msg["To"] = receiver_email
-
-        # Gmail SMTP connection (SSL version - more stable on Render)
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-            server.login(EMAIL, APP_PASSWORD)
-            server.send_message(msg)
-
-        print("OTP email sent successfully")
+        print("EMAIL SENT:", response.text)
         return True
 
     except Exception as e:
-        print("Email sending failed:", str(e))
+        print("EMAIL FAILED:", e)
         return False
