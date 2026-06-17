dfdd66d3e20d79cabc4ac6fce150d798826c2618
Saloni <saloninaagar08@gmail.com>
2026-06-17 15:13:28 +0530

    Fix Vercel CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,12 +25,12 @@ app = FastAPI(
     version="1.0.0"
 )
 
-# CORS (FIXED CLEAN VERSION)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://mindspark-pi-ten.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
